b660b6faac5c372113514b1d20fbf5e20ac03771
pennahall <135844443+pennahall@users.noreply.github.com>
2023-06-07 18:08:06 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -166,8 +166,8 @@ const RESOURCES = {"assets/AssetManifest.json": "dc05a2ff43c94f5ba9fc907aca0c177
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "91eeab86664a84dff9724eb80c54906a",
-"/": "91eeab86664a84dff9724eb80c54906a",
+"index.html": "0b0936f90549222dbdef3b35b5de1ac0",
+"/": "0b0936f90549222dbdef3b35b5de1ac0",
 "main.dart.js": "2adbf14cf59c70407ed27d085398165b",
 "manifest.json": "e9f7b0f370f2f30143fbde8a4f7ed524",
 "version.json": "26a31bd00fd2707a2d3cbd7882079ce7"};
